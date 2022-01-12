ed8249a3c923cf15b75d61af72000a24797494cf
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2022-01-12 10:57:25 +0200

    com

--- src/components/Card/Сard.js
+++ src/components/Card/Сard.js
@@ -2,10 +2,12 @@ import React from "react";
 export const Card = ({card}) => {
     console.log(card);
     return (
-        <div>
+        <div className={'flex'}>
+            <div>
             <h2>{card.mission_name}</h2>
             <p> - {card.launch_year}</p>
-            <img src={card.links.mission_patch_small} alt={card.mission_name}/>
+        </div>
+            <img className ={'img'} src={card.links.mission_patch_small} alt={card.mission_name}/>
         </div>
 
     )
