d92b88a26b5c200fa113330324540ba9b87dfc61
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2022-01-17 17:06:07 +0200

    com

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import {Comments} from "./components/Comments/Comments";
 export const App = () => {
     return (
         <div className={'father'}>
-            <div className={"flex"}>
-                <div className={'users'}><Users/></div>
 
-                <div className={'post'}><Posts/></div>
-            </div>
-           <div className={'comment'}> <Comments/></div>
         </div>
     );
 }
