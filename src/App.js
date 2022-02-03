53f4ccadfdbb21a342c04665a9ff0dacf20d6694
NastyaGrigorieva <nastya.grigoreva1502@gmail.com>
2022-02-03 14:04:20 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ const reduser = (state, action) => {
         case 'deleteCat' :
             return {...state, cats: state.cats.filter(cat => cat.id !== action.payload)}
         case 'addDog' :
-            return {...state, dogs: [...state.dogs, {name: action.payload,id: Date.now()}]}
+            return {...state, dogs: [...state.dogs, {name: action.payload, id: Date.now()}]}
         case 'deleteDog' :
             return {...state, dogs: state.dogs.filter(dog => dog.id !== action.payload)}
         default:
             return state
     }
 }
-
 
 export const App = () => {
     const [state, dispatch] = useReducer(reduser, {
@@ -46,7 +45,7 @@ export const App = () => {
                     <div>add Dog</div>
                     <input onChange={({target}) => setDogValue(target.value)}
                            type={'text'} value={dogValue}/>
-                    <button onClick={() => setNewAnimal( setDogValue,'addDog',  dogValue)}>вывести</button>
+                    <button onClick={() => setNewAnimal(setDogValue, 'addDog', dogValue)}>вывести</button>
                 </div>
             </div>
             <hr/>
@@ -55,14 +54,18 @@ export const App = () => {
                 <div>
                     Cats:
                     {
-                        state.cats.map(cat => <div key={cat.id}>{cat.name}<button onClick={() => dispatch({type: 'deleteCat', payload: cat.id})}>delete</button></div>)
+                        state.cats.map(cat => <div key={cat.id}>{cat.name}
+                            <button onClick={() => dispatch({type: 'deleteCat', payload: cat.id})}>delete</button>
+                        </div>)
                     }
                 </div>
 
                 <div>
                     Dogs:
                     {
-                        state.dogs.map(dog => <div  key={dog.id}>{dog.name}<button onClick={() =>dispatch({type: 'deleteDog',payload: dog.id})}>delete</button></div>)
+                        state.dogs.map(dog => <div key={dog.id}>{dog.name}
+                            <button onClick={() => dispatch({type: 'deleteDog', payload: dog.id})}>delete</button>
+                        </div>)
                     }
                 </div>
             </div>
